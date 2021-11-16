7c9af27979f5eaa1b56dfca17d4694dcac71d4e6
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-16 13:52:39 -0600

    Un ejemplo mas, cambiando el tamaño de la fuente a un parrafo mediante 3 botones utilizando el metodo getElementById del objeto document

---
+++ js/02.getElementById_2.js
@@ -0,0 +1,13 @@
+console.log("llegué");
+
+function fuentePequena(){
+    document.getElementById('parrafo').style.fontSize = "15px"
+}
+
+function fuenteMediana(){
+    document.getElementById('parrafo').style.fontSize = "20px"
+}
+
+function fuenteGrande(){
+    document.getElementById('parrafo').style.fontSize = "23px"
+}
