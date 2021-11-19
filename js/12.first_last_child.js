54ac212cc5059c800ab8d86e0a72d188edf149aa
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-18 18:37:10 -0600

    Obteniendo a los elementos hermanos partiendo del primer o ultimo hijo de un elemento html

---
+++ js/12.first_last_child.js
@@ -0,0 +1,45 @@
+console.log("Llegué");
+
+function obtenerParrafo(){
+    let puntero1 = document.getElementById("parrafos");
+    let puntero2 = puntero1.firstChild;
+
+    while ( puntero2 != null) {
+
+        if ( puntero2.nodeType == Node.ELEMENT_NODE) { 
+            console.log( `Nodo tipo: "${puntero2.nodeName}": su valor es: "${puntero2.childNodes[0].nodeValue}"`);
+        }else {
+            console.log( `Nodo tipo "${puntero2.nodeName}": No es un nodo tipo elementos, si no de texto`);
+        }
+
+        puntero2 = puntero2.nextSibling;
+        
+    }
+}
+
+function obtenerElementosParrafo() {
+    let puntero1 = document.getElementById("parrafos");
+    let puntero2 = puntero1.firstElementChild;
+
+    while ( puntero2 != null) {
+        console.log(puntero2.childNodes[0].nodeValue)
+        puntero2 = puntero2.nextElementSibling;
+    }
+
+    console.log("====== o bien: =======");
+    let fchild = document.getElementById("parrafos").firstElementChild.innerHTML;
+    let lchild = document.getElementById("parrafos").lastElementChild.innerHTML;
+
+    console.log(`${fchild}`);
+    console.log(`${lchild}`);
+}
+
+function getParagraphs() {
+    let container = document.getElementById("container");
+    let parrafos = container.lastElementChild;
+
+    while (parrafos != null) {
+        console.log(parrafos.childNodes[0].nodeValue);
+        parrafos = parrafos.previousElementSibling;
+    }
+}
