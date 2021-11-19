011c732f08c350ec03910c25927c9213c0c8aa28
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-18 19:58:02 -0600

    trabajando con document.getElementByTagName()

--- js/14.getElementsByTagName.js
+++ js/14.getElementsByTagName.js
@@ -6,13 +6,20 @@ let obtenerParrafos = function () {
     for (let i = 0; i < array.length; i++) {
         
         //array[i].childNodes[0].nodeValue = array[i].childNodes[0].nodeValue + " ";
-        
+
         console.log(array[i].childNodes[0].nodeValue);
 
         array[i].style.fontSize = "25px";
         array[i].style.backgroundColor = "silver";
+        
+    }
+}
 
-        
-        
+let colorearParrafos = function () {    
+    let container = document.getElementById("container-1");
+    
+
+    for (let i of arrayParrafos) {
+        arrayParrafos[i].style.color = "red";
     }
 }
