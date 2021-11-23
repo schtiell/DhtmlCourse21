48e64d670459c2c85b75abc0fdff0800e5d676e7
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-22 19:26:38 -0600

    gitElementsByTagName() algunos ejemplos aunque aun es necesario practicar mas para comprender el correcto funcionamiento

--- js/14.getElementsByTagName.js
+++ js/14.getElementsByTagName.js
@@ -15,11 +15,38 @@ let obtenerParrafos = function () {
     }
 }
 
-let colorearParrafos = function () {    
-    let container = document.getElementById("container-1");
-    
 
-    for (let i of arrayParrafos) {
-        arrayParrafos[i].style.color = "red";
+//Funcion para el ejercicio 15.getElementsByTagName.html
+let colorearParrafos = function () {    
+    
+    let container = document.getElementById("container-1");
+
+    let parrafos = container.getElementsByTagName('p');
+
+        console.log(parrafos);
+
+    for (const element of parrafos) {
+
+        element.style.background = "red";
+        
+    }
+    
+}
+
+var esconderParrafos = function () {
+    
+    let container = document.getElementById("container-2");
+
+    let parrafos = container.getElementsByTagName("p");
+
+    console.log(container)
+
+
+
+    for (const p of parrafos) {
+
+        console.log(`${container.childElementCount } - ${p.innerHTML}`);
+
+        p.style.display = "none";
     }
 }
