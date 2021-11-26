a763b5eea3851776707ad5e281b6899f2612468f
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-26 12:50:12 -0600

    eliminando atributos de los elementos html con js

--- js/26.agregarAtributo.js
+++ js/26.agregarAtributo.js
@@ -17,9 +17,30 @@ let asignarAtributo = function () {
     //Agregando el ancla al contenedor padre
     contenedor.appendChild(link);
 
-    //Definiendo un atributo para la nueva ancla html creada
+    /* Definiendo un atributo para la nueva ancla html creada
+    El primer parámetro es el nombre de la propiedad (en este caso href) y el segundo es el valor que toma la propiedad */
     link.setAttribute("href","http://www.google.com");
 
     //Imprimiendo el valor de link en consola
     console.log(`${link}`);
+}
+
+let aplicarAtributos = function () {
+
+    //console.log("bordes dibujados");
+
+    let atributosTabla = document.getElementById("tabla");
+
+    //Definicion de algunos atributos para tablas, color, borde y aleamiento
+    atributosTabla.setAttribute("border","2");
+    atributosTabla.setAttribute("bordercolor","#000");
+    atributosTabla.setAttribute("align","center");
+    atributosTabla.setAttribute("cellpadding","10px");
+    atributosTabla.setAttribute("width","50%");
+        
+    console.log(atributosTabla.childNodes);
+
+    //Aplicando estilo de color de fondo para la columna de encabezados
+    atributosTabla.childNodes[1].style.background ="silver";
+
 }
---
+++ js/28.removerAtributo.js
@@ -0,0 +1,39 @@
+
+
+let eliminarAtributos = function () {
+
+    let enlace = document.getElementById("google");
+
+    let enlacefb = document.getElementById("fb");
+
+    enlace.removeAttribute("href");
+
+    enlacefb.removeAttribute("href");
+
+    console.log("Atributos removidos");
+
+}
+
+let agregarAtributos = function () {
+
+    let enlace = document.getElementById("google");
+
+    let enlacefb = document.getElementById("fb");
+
+    enlace.setAttribute("href","http://google.com");
+
+    enlacefb.setAttribute("href","http://facebook.com");
+
+    console.log("Atributos agregados");
+
+}
+
+let eliminarBorde = function () {
+
+    let tabla = document.getElementById("tabla");
+
+    tabla.removeAttribute("border");
+
+    console.log("Atributo removido");
+
+}
