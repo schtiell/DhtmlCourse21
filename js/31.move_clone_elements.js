290e2876d39878b3830446499f16d63c7a7f7827
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-29 11:51:51 -0600

    clonando y moviendo elementos html

--- js/31.move_clone_elements.js
+++ js/31.move_clone_elements.js
@@ -29,3 +29,36 @@ let clonarElementos = function () {
     lista2.appendChild(clonLista);
 
 }
+
+//Funciones del archivo 31.move_clone_node.html
+function moveElements() {
+    //console.log("moviendo elementos");  
+    
+    let parrafos = document.getElementById("container1");
+
+    let parrafoIndividual = parrafos.firstChild;
+
+    if (parrafos.childNodes.length > 0) {
+
+        let contenerdorDestino = document.getElementById("container2");
+
+        contenerdorDestino.appendChild(parrafoIndividual);
+    }else{
+        console.log("No hay mas elementos que mover");
+    }
+
+}
+
+function cloneElements() {
+
+    //console.log("clonando elementos");   
+
+    let container = document.getElementById("container1");
+
+    let clnElementos = container.cloneNode(true);
+
+    let containerDestino = document.getElementById("container2");
+
+    containerDestino.appendChild(clnElementos);
+
+}
