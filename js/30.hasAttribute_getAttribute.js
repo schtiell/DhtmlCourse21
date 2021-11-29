2546cda944b7d9a6bf1ef4ecae84570b9ce443e3
joaquin sayago <isc.jsayago@hotmail.com>
2021-11-29 09:48:21 -0600

    se utilizan la funcion hasAttribute() para saber si un elemento tiene atributos, ademas se utiliza removeAttribute() para eliminar un atributo especifico y setAttribute par asignar un atributo y su valor

--- js/30.hasAttribute_getAttribute.js
+++ js/30.hasAttribute_getAttribute.js
@@ -8,6 +8,14 @@ let getAttributes = function () {
     if(tabla.hasAttribute.length > 0){
         console.log(`El elemento tabla tiene ${tabla.hasAttribute.length} atributos`);
         console.log(`El atributo es un borde con valor :${tabla.getAttribute("border")}`);
+
+        //Si tienen un atributo de borde entonces lo eliminar
+        if (tabla.hasAttribute("border")){
+            tabla.removeAttribute("border");
+        }else{
+            //Si no tiene el borde entonces le asigna uno con un ancho de 5px
+            tabla.setAttribute("border", "5px");
+        }
     }
 
     //tabla.getAttributeNames()
