17e07afa231b60ceb3c461cd7b0388ae1ee25239
joaquin sayago <isc.jsayago@hotmail.com>
2021-12-03 19:26:16 -0600

    evento onclick

---
+++ js/39.eventoonclick.js
@@ -0,0 +1,14 @@
+
+
+let clicParrafo = function (){
+    console.log("Se dió clic al parrafo");
+
+}
+
+let clicCelda = function () {
+    console.log("Se dió clic sobre una celda");
+}
+
+let clicBotton = function () {
+    console.log("Se hizo clic en el boton");
+}
