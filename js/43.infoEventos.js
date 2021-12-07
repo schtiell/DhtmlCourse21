277fd63d41953458420215bb4913de6492b64519
joaquin sayago <isc.jsayago@hotmail.com>
2021-12-07 17:39:46 -0600

    Trabajando con la informacion generada por un evento onclick

--- js/43.infoEventos.js
+++ js/43.infoEventos.js
@@ -9,3 +9,18 @@ let presionado = function (e){
     //la propiedad clientX y clientY permite ubicar en que posicion x y y se dio clic a traves del evento onclick en html
     console.log(`La coordenada x del puntero del mouse es: ${e.clientX}, la coordenada en y es ${e.clientY}`);
 }
+
+
+let presionandoParrafo = function (e) {
+
+    console.log("se presionó el parrafo");
+
+    let contenedor = document.getElementById("ayuda");
+
+    console.log(`La coordenada x del puntero del mouse es: ${e.clientX}, la coordenada en y es ${e.clientY}`);
+
+    //Creando un objeto 
+    contenedor.style.display = "block";
+    contenedor.style.left = e.clientX + 10 + "px";
+    contenedor.style.top = e.clientY + 10 + "px";
+}
