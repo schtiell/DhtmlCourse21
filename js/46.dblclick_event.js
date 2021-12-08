f55ebd9a9435d39e9316b562f8ba42cd2b6d838b
joaquin sayago <isc.jsayago@hotmail.com>
2021-12-08 11:05:43 -0600

    ejemplo de redimension de elemento con el evento ondblclick y onclik

--- js/46.dblclick_event.js
+++ js/46.dblclick_event.js
@@ -4,5 +4,23 @@ let ocultarContenedor = function (objeto) {
     
     console.log("Elemento oculto" + objeto);
 
+    //Display = "none" permite oculta el div
     objeto.style.display = "none";
+}
+
+
+let redimensionarElemento = function (objeto) {
+
+    console.log("Elemento redimensionado");
+
+    objeto.style.width = 250 + "px";
+    objeto.style.height = 250 + "px";
+}
+
+let retornarTamanoElemento = function (objeto) {
+
+    console.log("Elemento redimensionado");
+
+    objeto.style.width = 800 + "px";
+    objeto.style.height = 150 + "px";
 }
