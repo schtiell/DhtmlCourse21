dc2e52c681da3e85a8dc0fc2df67985919e550e7
joaquin sayago <isc.jsayago@hotmail.com>
2021-12-09 17:15:40 -0600

    eventos onload y unonload para ejecutar scripts de js una vez que el sitio ha cargado completamento o bien una vez que se ha abandonado el mismo

---
+++ js/50.onload_onunload.js
@@ -0,0 +1,12 @@
+
+//Al cargar el sitio se ejecuta la función que muestra un alert en pantalla con la url actual del sitio
+let imprimirMensaje = function () {
+
+    alert(window.location.href);
+    alert(window.location.pathname);
+}
+
+let imprimirDespedida = function () {
+    
+    alert("Adios, regresa pronto");
+}
