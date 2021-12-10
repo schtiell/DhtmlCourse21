fcf3e43398fec252edf7e30cb336a1f4c16dad74
joaquin sayago <isc.jsayago@hotmail.com>
2021-12-09 18:57:20 -0600

    evento click del lado de la funcionalidad del sitio con js sustituye a onclick del lado de html, tal y como recomienda la w3c

--- js/51.eventos_DOM_W3C.js
+++ js/51.eventos_DOM_W3C.js
@@ -1,6 +1,9 @@
 
 //De esta manera se separa por completo la funcionalidad del sitio (js) de la estructura de la pagina (html).
 
+//  ----------------------  1era forma de hacerlo -----------------------------------
+
+/*
 //Se invoca el elemento mediante el id
 let boton1 = document.getElementById("id-btn1");
 
@@ -17,3 +20,38 @@ boton2.addEventListener("click", () => {
     let boton2 = document.getElementById("id-btn2");
     alert(boton2.getAttribute("value"));
 });
+
+
+/* --------------------------------- 2da forma de hacer lo anterior ---------------------------
+
+let boton1 = document.getElementById("id-btn1");
+
+let imprimirValor = function () {
+
+    let boton1 = document.getElementById("id-btn1");
+    alert(boton1.getAttribute("value"));
+
+}
+boton1.addEventListener("click", imprimirValor);
+
+
+let boton2 = document.getElementById("id-btn2");
+boton2.addEventListener("click", imprimirValor2);
+
+function imprimirValor2 () {
+    let boton2 = document.getElementById("id-btn2");
+    alert(boton2.getAttribute("value"));
+} */
+
+
+// ------------------------- 3era forma: RESUMIDA ------------------------------------
+
+document.getElementById("id-btn1").addEventListener("click", ()=>{
+    alert(document.getElementById("id-btn1").getAttribute("value"));
+});
+
+document.getElementById("id-btn2").addEventListener("click", function () {
+    alert(document.getElementById("id-btn2").getAttribute("value"));
+});
+
+
---
+++ js/52.event_click_w3c.js
@@ -0,0 +1,4 @@
+
+document.getElementById("enlace").addEventListener("click", ()=>{
+    alert("Gracias por visitar el sitio");
+})
