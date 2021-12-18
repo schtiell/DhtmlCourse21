427e6a877eecbdccbbb921f00dc75efdb963582f
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2021-12-18 00:03:11 -0600

    agregando comentarios en el file 69 js

--- js/69.desactivar_evento_pordefecto.js
+++ js/69.desactivar_evento_pordefecto.js
@@ -1,7 +1,11 @@
 
 
 let prevenirEvento = function (e) {
+
+    //Evita que se dispare el comportamiento por default de un enlace que es abrir la liga definida en el atributo href
     e.preventDefault();
+
+    //Obtiene la url del href y la abre en una nueva pestaña utilizando window.open(url)
     let url = e.target.getAttribute("href");
     window.open(url);
 }
