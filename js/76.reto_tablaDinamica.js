ba9b3f2ec701ae9c979288f6351ba9124ba0fd0d
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2021-12-19 23:55:52 -0600

    generando una tabla dinamica a partir de un arreglo de objetos. la tabla muestra coincidencias de nombres escritos en un textbox utilizando el metodo strartsWith mediante un evento keyup

--- js/76.reto_tablaDinamica.js
+++ js/76.reto_tablaDinamica.js
@@ -1,8 +1,34 @@
-console.log("tabla dinamica, reto 1");
+//console.log("tabla dinamica, reto 1");
+
+
+let generarTabla = function (palabra) {
+
+    let tabla = `<table>`;
+    tabla +=    `<tr>
+                    <th>Nombre</th>
+                    <th>Edad</th>
+                </tr>`;
+
+    for (const persona of personas) {
+
+            if (palabra == undefined || persona.nombre.startsWith(palabra)){
+                tabla +=    `<tr>
+                                <td>${persona.nombre}</td>
+                                <td>${persona.edad}</td>
+                            </tr>`;
+            }
+        }
+
+    tabla += `</table>`;
+
+};
+
+document.querySelector("#textBox-id").addEventListener("keyup", () => {
+    generarTabla(document.querySelector("#textBox-id").value);
+})
 
 
 //Generar una tabla dinamica a partir del siguente arreglo de objetos, cada objeto contiene el nombre y la edad de las personas
-
 const personas = [
     {
         nombre: "dardo",
@@ -37,3 +63,5 @@ const personas = [
         edad: 90
     }
 ]
+
+generarTabla();
