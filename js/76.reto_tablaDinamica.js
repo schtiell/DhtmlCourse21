9623f7c2f982e030128f3c628df1c66d44128133
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2021-12-19 23:16:49 -0600

    Iniciando el reto 1 del curo de DHTML que consiste en generar una tabla dinamica a partir de un array de objetos

---
+++ js/76.reto_tablaDinamica.js
@@ -0,0 +1,39 @@
+console.log("tabla dinamica, reto 1");
+
+
+//Generar una tabla dinamica a partir del siguente arreglo de objetos, cada objeto contiene el nombre y la edad de las personas
+
+const personas = [
+    {
+        nombre: "dardo",
+        edad: 52
+    },
+    {
+        nombre: "pedro",
+        edad: 522
+    },
+    {
+        nombre: "ana",
+        edad: 89
+    },
+    {
+        nombre: "alicia",
+        edad: 55
+    },
+    {
+        nombre: "carlos",
+        edad: 21
+    },
+    {
+        nombre: "capitanich",
+        edad: 89
+    },
+    {
+        nombre: "carla",
+        edad: 66
+    },
+    {
+        nombre: "altina",
+        edad: 90
+    }
+]
