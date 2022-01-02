0988dd05732507344b4e01bca22230d806615f32
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2022-01-01 22:22:55 -0600

    Se terminó el ejemplo sobre el consumo de la API para las banderas, aunque hay algunos datos de objetos anidados que aun no han sido resueltos por lo que debo investigar mas al respecto

--- js/78.reto3_consumirApiBanderas.js
+++ js/78.reto3_consumirApiBanderas.js
@@ -1,10 +1,19 @@
 console.log("consumiendo api");
 
+//La función fetch recibe la dirección de la api y retorna un archivo JSON
 fetch("https://restcountries.com/v3.1/all")
+
+    //Recibido los datos se convierten en un objeto JSON
     .then(response => response.json())
+
+    //Con la promesa devuelta por el metodo then se recibe la información en formato json y se recorre para obtener los datos necesarios de la API
     .then( datos => {
+
+        //Cadena vacia
         let banderas = " ";
+
         for (const pais of datos){
+
             banderas += `<div class="tarjeta">
                             <img src="${pais.flags.png}"><br>
                             <p>Pais: ${pais.name.official}</p>
@@ -14,7 +23,12 @@ fetch("https://restcountries.com/v3.1/all")
                         </div>`;
         }
 
+        //S invoca al elemento HTML a traves del selector css por id
         const elemento = document.querySelector("#banderas");
+
+        //Finalmente escribe en el DOM el contenido de la la cadena llamada banderas
         elemento.innerHTML = banderas;
     })
+
+    //Muestra en consola si ocurre un error al llamar a la API
     .catch(error => console.log(error));
