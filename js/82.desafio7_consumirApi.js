c196bb4612fa8b147eaf00392b507838dbc4c010
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2022-01-14 18:22:34 -0600

    Se agregaron algunos comentarios de apoyo en el ejemplo de la api daylimotion.com

--- js/82.desafio7_consumirApi.js
+++ js/82.desafio7_consumirApi.js
@@ -1,11 +1,15 @@
 console.log("desafio 8");
 
+//Al cargar el sitio completamente se llama a la api a traves de la función fetch
 fetch("https://api.dailymotion.com/videos?limit=100")
+
+    //El metodo recibe los datos del servidor y son convertidos en formato json
     .then(response => response.json())
     .then(datos => {
 
         let cadena = '';
 
+        //Con los datos recibidos se muestran los datos de cada video generados dinamicamente.
         for (const registro of datos.list) {
             cadena += ` <div class="tarjeta">
                             <a target="_blank" href="https://www.dailymotion.com/video/${registro.id}">
@@ -14,6 +18,8 @@ fetch("https://api.dailymotion.com/videos?limit=100")
                             <br>
                             <p>${registro.title}</p>
                         </div>`;
+
+            //Finalmente se escriben los datos en el DOM dentro del elemento con id datos
             document.querySelector("#datos").innerHTML = cadena;
         }
     })
