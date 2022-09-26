7098872fc2daba1256fff1f88ad2f4ac87c5cd2c
jsayagot <isc.jsayago@hotmail.com>
2022-09-26 14:37:47 -0500

    Aplicando estilos y clases bootstrap al ejercicio 04, ademas repasando las propiedades childNode y children para conocer el tipo y el numeor de nodos de un elemento del DOM

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ El siguiente apartado hace una descripción de cada uno de los ejercicios realiz
 1. 02.getElementById.html, el cual llama a un elemento para cambiar el color y tamaño de de fuente del titulo
 
 2. 03.getElementById_2.html, encargado de buscar un elemento con id parrafo para aplicarle distintos tamaños de fuentes
+
+3. 04.childNodes_children.html, se utilizan las propiedades childNode para conocer el numero de hijos que posee el elemento padres (nodos de texto y elementos), mientras que children devuelve unicamente le numero de elemento hijos
--- js/03.childNodes_children.js
+++ js/03.childNodes_children.js
@@ -1,18 +1,19 @@
 console.log('Propiedad childNodes & children');
 
 function calcularHijos(){
-    let hijos = document.getElementById("container");
+
+    let hijos = document.getElementById("contenedorPadre");
 
     //Muestra en consola 7 ya que son 3<p> y 4 saltos de linea
-    console.log(hijos);
-    console.log(`La cantidad de hijos es ${hijos.childNodes.length}`);
-
     let cadena = "";
+    let nodos_texto = 0;
 
     for (let i = 0; i < hijos.childNodes.length; i++) {
         
+        //childNodes, devuelve nodos de texto y nodos elementos
         if (hijos.childNodes[i].nodeType == Node.TEXT_NODE) {
             cadena = cadena + 'Nodo tipo texto \n';
+            nodos_texto +=1;
         }
 
         if (hijos.childNodes[i].nodeType == Node.ELEMENT_NODE) {
@@ -21,10 +22,12 @@ function calcularHijos(){
     }
 
     console.log(`${cadena}`);
+    console.log(`El numero de nodos texto es: ${nodos_texto}, ya que los saltos de linea son considerados nodos de texto`);
+
 }
 
-
-function calcularElementos() {
-    let elementos = document.getElementById('container');
-    console.log(`El numero de elementos es ${elementos.children.length}`);
+// La propiedad children retorna unicamente el numero de elementos de un nodo
+function calcularNodosElementos() {
+    let elementos = document.getElementById('contenedorPadre');
+    console.log(`El numero de nodos elemento es: ${elementos.children.length}`);
 }
