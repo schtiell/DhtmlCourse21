d7319ead2ec3e7b8eca12c6213cd46ee2e54bd47
jsayagot <isc.jsayago@hotmail.com>
2022-10-07 19:54:06 -0500

    Trabajando en el ejercicio 56.mouseover_mouseout

--- js/56.mouseover_mouseout.js
+++ js/56.mouseover_mouseout.js
@@ -1,43 +1,45 @@
-console.log("eventos mouseover & mouseout");
-
 
 let entrar = function (e) {
 
     //Si el nombre de etique es un button entonces aplica el background  rojo
     if (e.target.tagName === "BUTTON"){
-        e.target.style.background = "#EC7063";
+
+        let boton = e.target;
+        boton.removeAttribute("class");
+        boton.setAttribute("class","btn btn-warning");
+
     }else{
-        e.target.style.background = "#F2F4F4";
+        e.target.style.background = "#85C1E9";
     }
 }
 
 let salir = function (e) {
     
+
     if (e.target.tagName === "BUTTON"){
-        e.target.style.background = "#0275d8";
+
+        let boton = e.target;
+        boton.removeAttribute("class");
+        boton.setAttribute("class","btn btn-primary");
+
     }else{
-        e.target.style.background = "#FDFEFE"
+        e.target.style.background = "#343A40";
     }
 }
 
 
 
-let array = document.getElementsByTagName("button");
-console.log(array.length);
+let botones = document.getElementsByTagName("button");
+let celdas = document.getElementsByTagName("td");
 
-for (let i = 0; i < array.length; i++) {
-    array[i].addEventListener("mouseover", entrar);
-    array[i].addEventListener("mouseout", salir);   
-    console.log(array[i]);
+for (let i = 0; i < botones.length; i++) {
+    botones[i].addEventListener("mouseover", entrar);
+    botones[i].addEventListener("mouseout", salir);   
 }
 
-//Busca todas las etiquetas td y las almacena en un arreglo
-let arrayCeldas = document.getElementsByTagName("td");
-console.log(arrayCeldas.length);
 
-//Recorre el arreglo para aplicar el metodo addEventListener
-for (let i = 0; i < arrayCeldas.length; i++) {
-    arrayCeldas[i].addEventListener("mouseover", entrar);
-    arrayCeldas[i].addEventListener("mouseout", salir);
+for (let i = 0; i < celdas.length; i++) {
+    celdas[i].addEventListener("mouseover", entrar);
+    celdas[i].addEventListener("mouseout", salir);
     
 }
