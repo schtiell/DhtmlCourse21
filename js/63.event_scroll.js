9f0de8b6d54e7bf5fbe75b054d5b5d3954f3c7d2
jsayagot <isc.jsayago@hotmail.com>
2022-10-10 12:56:14 -0500

    Evento scroll el cual muestra en pantalla los pixeles del movimiento en pantalla

--- js/63.event_scroll.js
+++ js/63.event_scroll.js
@@ -1,11 +1,12 @@
 
+// Funcion auto ejecutable
 (function () {
     console.log("Evento scroll");
 })();
 
-window.addEventListener("scroll", e => {
-    let lista = document.getElementById("containerlist");
-    lista.style.top = window.scrollY + "px";
-    lista.style.left = window.scrollX + "px";
-    console.log(lista.style.top = window.scrollY + "px");
+window.addEventListener("scroll", () => {
+    let scrolling = document.getElementById("containerlist");
+    scrolling.style.top = window.scrollY + "px";
+    scrolling.style.left = window.scrollX + "px";
+    console.log(scrolling.style.top = window.scrollY + "px");
 })
