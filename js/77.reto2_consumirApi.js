c23686244f49790f4237a96bafdb80d634423b08
jsayagot <isc.jsayago@hotmail.com>
2022-10-13 19:55:46 -0500

    Decifrando el metodo fetch para trabajar con APIs

--- js/77.reto2_consumirApi.js
+++ js/77.reto2_consumirApi.js
@@ -1,4 +1,3 @@
-//console.log("consumiendo api");
 
 //https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data-es
 
@@ -8,25 +7,43 @@ function recuperarPrecio(){
 
     //Fetch API, permite interactuar con APIs y obtener los datos a nuestra aplicación, de manera sencilla
     
-    //El único parámetro requerido de fetch() es una url. El método por defecto en este caso es GET.
+    //El único parámetro requerido por el metodo fetch() es una url. El método por defecto en este caso es GET.
     fetch("https://api.coincap.io/v2/assets/bitcoin")
+
+        // con arrow funtion .then(respuest => respuesta.json())
+
+        // el metodo then
+
+        // el parametro respuesta roma el valor devuelto del metodo fetch(url), sin embargo es un objeto por lo tanto se debe convertir a un objeto  JSON
         .then(respuesta => respuesta.json())
         
-        .then(datos => {
-            console.log(datos);
-            document.querySelector("#precio").innerHTML = parseInt(datos.data.priceUsd);
+        /*convertida la respuesta a JSON, los datos deben ser procesados, por lo que nuevamente se llama al metodo then() */
+        .then( datos => {
 
-            if(parseFloat(datos.data.changePercent24Hr) > 0){
+            console.log(datos);
+
+            let precio = document.querySelector("#precio");
+
+            let valor = datos.data;
+
+            precio.innerHTML = parseInt(valor.priceUsd);
+
+            if(parseFloat(valor.changePercent24Hr) > 0){
+
                 document.querySelector("#variacion").style.color = "green";
-                document.querySelector("#variacion").innerHTML = "↑" + parseFloat(datos.data.changePercent24Hr).toFixed(2);
+                document.querySelector("#variacion").innerHTML = "↑" + parseFloat(valor.
+                changePercent24Hr).toFixed(2);
+
             }else{
+
                 document.querySelector("#variacion").style.color = "red";
                 document.querySelector("#variacion").innerHTML = "↓" + parseFloat(datos.data.changePercent24Hr).toFixed(2);
             }
         })
+
         .catch(error => console.log(error))
 
-        /* PUEDE HACERSE ASI TAMBIEN PERO COMO SOLO ES UN LINEA DEL CUERPO DE LA FUNCION PUEDEN OMITIRSE LAS LLAVES DE APERTURA Y CIERRE DE LA FUNCIÓN
+        /* o bien, pero como es una sola linea la arrow function permite omitir las llaves
             .catch(error => {
                 console.log(error)
             })
